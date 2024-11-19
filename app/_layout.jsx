749ba6a7e61b6158f5c45de0e148614eb79b08e4
Jamaica-1234 <genonjamaica9@gmail.com>
2024-11-19 15:10:45 +0800

    First Commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -35,6 +35,8 @@ const MainLayout = () => {
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false }}/>
       <Stack.Screen name="(tabs)" options={{headerShown: false }}/>
+      <Stack.Screen name="login" options={{headerShown: false }}/>
+      <Stack.Screen name="signup" options={{headerShown: false }}/>
     </Stack>
   )
 }
